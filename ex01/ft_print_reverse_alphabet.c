561a16e5c767c4b448cd87b6c776ada9cea9ff87
ismlambo <ismlambo@student.wethinkcode.co.za>
2020-07-12 13:18:59 +0200

    done

---
+++ ex01/ft_print_reverse_alphabet.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ismlambo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/12 13:11:40 by ismlambo          #+#    #+#             */
+/*   Updated: 2020/07/12 13:12:02 by ismlambo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include<unistd.h>
+
+void ft_putchar(char c);
+
+void ft_print_reverse_alphabet(void){
+    char start;
+    char end;
+
+    end = 'a';
+    start = 'z';
+    while (start >= end){
+        ft_putchar(start);
+        start = start - 1;
+    }
+}
+
+void ft_putchar(char c){
+    write(1, &c , 1);
+}
+
+int main(void){
+    ft_print_reverse_alphabet();
+    return(0);
+}
